0f018d4a785c65156bd42550767eae138eaa8e24
Ibnu Aziz Nurrohman <32543664+IbnuAziz@users.noreply.github.com>
2022-02-04 16:43:37 +0700

    Update shopping.js

--- api/models/shopping.js
+++ api/models/shopping.js
@@ -4,7 +4,6 @@ const shoppingSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	name: {type: String, required: true},
 	createDate: {type: String, required: true},
-	shoppingImage : { type: String, reqired: true}
 });
 
 module.exports = mongoose.model('Shopping', shoppingSchema);

543a5c80838578a7519873f63453d0df43c45939
Ibnu Aziz Nurrohman <32543664+IbnuAziz@users.noreply.github.com>
2022-02-04 16:43:06 +0700

    Update shoppings.js

--- api/routes/shoppings.js
+++ api/routes/shoppings.js
@@ -3,30 +3,30 @@ const router = express.Router();
 const multer = require('multer');
 
 
-const storage = multer.diskStorage({
-	destination: function(req, file, cb){
-		cb(null,  './uploads/');
-	},
-	filename: function(req, file, cb){
-		cb(null, file.originalname);
-	}
-});
+// const storage = multer.diskStorage({
+// 	destination: function(req, file, cb){
+// 		cb(null,  './uploads/');
+// 	},
+// 	filename: function(req, file, cb){
+// 		cb(null, file.originalname);
+// 	}
+// });
 
-const fileFilter = (req, file, cb) =>{
-	if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
-		cb(null, true);
-	}else{
-		cb(null, false);
-	}
-}
+// const fileFilter = (req, file, cb) =>{
+// 	if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+// 		cb(null, true);
+// 	}else{
+// 		cb(null, false);
+// 	}
+// }
 
-const upload = multer({
-	storage: storage, 
-	limits: {
-		fileSize: 1024 * 1024 * 5
-	},
-	fileFilter: fileFilter
-});
+// const upload = multer({
+// 	storage: storage, 
+// 	limits: {
+// 		fileSize: 1024 * 1024 * 5
+// 	},
+// 	fileFilter: fileFilter
+// });
 
 
 const checkAuth = require('../middleware/check-auth');
@@ -34,7 +34,7 @@ const ShoopingController = require('../controllers/shoppings');
 
 router.get('/', ShoopingController.shoppings_get_all);
 
-router.post ('/', checkAuth, upload.single('shoppingImage'), ShoopingController.shoppings_create);
+router.post ('/', checkAuth, ShoopingController.shoppings_create);
 
 router.get('/:shoppingId', ShoopingController.shoppings_get_byId);
 
